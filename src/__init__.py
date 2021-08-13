11a084c8d9ff2a34bca2d4c7de383430c6472608
moonburnt <moonburnt@disroot.org>
2021-08-13 07:30:51 +0300

    feat: basic anti-overwrite protection
    - Added basic protection from overwriting files with same names by
    adding uuid4 to them. This should work relatively well for now
    - Moved filesaving-related functionality to save_file()

--- src/__init__.py
+++ src/__init__.py
@@ -11,9 +11,11 @@ from flask import (
     wrappers,
 )
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 from os.path import join, splitext
 from os import environ, makedirs
 from sys import exit
+from uuid import uuid4
 import logging
 
 log = logging.getLogger(__name__)
@@ -21,6 +23,7 @@ log = logging.getLogger(__name__)
 # TODO: maybe make it possible to load settings from config.ini/config.toml
 def create_app() -> Flask:
     """Create and configure site's flask instance"""
+
     app = Flask(__name__, instance_relative_config=True)
     try:
         app.config.from_mapping(
@@ -46,13 +49,31 @@ def create_app() -> Flask:
 
     def allowed_file(filename: str) -> bool:
         """Check if file can be uploaded, based on its extension"""
+
         # #TODO: add hashsum blacklist or something like that, make function check
         # against it too
         return splitext(filename)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
 
+    def save_file(file: FileStorage, filename: str = None) -> str:
+        """Save provided file into server's UPLOAD_DIRECTORY"""
+
+        # Ensuring filename wont contain unwanted symbols
+        # this may crash without file.filename, but it shouldnt happen
+        filename = secure_filename(filename or file.filename)
+        # We could avoid this part entirely by adding uuid before name
+        # But for now it doesnt seem like an issue, may change l8r #TODO 
+        name, extension = splitext(filename)
+        # Adding uuid4 to name, to avoid overwriting existing files with same name
+        filename = f"{name}-{uuid4()}{extension}"
+        # #TODO: add user-specific subdirectories, save into them based on id
+        file.save(join(app.config["UPLOAD_DIRECTORY"], filename))
+
+        return filename
+
     @app.route("/uploads/<path:name>", methods=["GET", "POST"])
     def download_file(name: str) -> wrappers.Response:
-        """Get link to download file with provided name from server's UPLOAD_DIRECTORY"""
+        """Get link to download specified file from server's UPLOAD_DIRECTORY"""
+
         # Toggling "as_attachment" will make browsers open "save file" dialog instead
         # of opening direct link in new tab
         # return send_from_directory(app.config["UPLOAD_DIRECTORY"], name, as_attachment=True)
@@ -61,6 +82,7 @@ def create_app() -> Flask:
     @app.route("/", methods=["GET", "POST"])
     def upload_file():
         """File uploading form"""
+
         if request.method == "POST":
             if "file" not in request.files:
                 flash("Received no files")
@@ -73,9 +95,7 @@ def create_app() -> Flask:
                 return redirect(request.url)
 
             if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(join(app.config["UPLOAD_DIRECTORY"], filename))
-                return redirect(url_for("download_file", name=filename))
+                return redirect(url_for("download_file", name=save_file(file)))
 
         return render_template("upload_file.html.jinja")
 
