e01ab0d9aa4fd89a2f6fedec629171805c6d719b
moonburnt <moonburnt@disroot.org>
2021-08-28 22:49:56 +0300

    feat: personal uploads
    - In case user is logged in, their files will be uploaded into
    user-specific directory (right now its set to user's login)
    - Login of uploader (in case exists) is now saved into db

--- src/filesharing.py
+++ src/filesharing.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     flash,
     render_template,
+    abort,
 )
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
@@ -32,12 +33,16 @@ class FileInfo:
     filename: str
     # name of file how its been received (after securing but without uuid4)
     original_name: str
-    # not implemented yet #TODO
-    # uploader: str
     # time of file's upload
     uploaded: datetime
     # when file was accessed the last time
     last_access: datetime
+    # login of user who uploaded file. Will be None in case of anonymous upload
+    uploader: str = None
+    # subdirectory where file is saved on disk. Is either None or user's login.
+    # Kept as separate value to be future-proof, in case it will be switched
+    # to uploader's id or something like that later
+    location: str = None
 
 
 def allowed_file(filename: str) -> bool:
@@ -59,51 +64,85 @@ def save_file(file: FileStorage, filename: str = None) -> str:
     name, extension = splitext(filename)
     # Adding uuid4 to name, to avoid overwriting existing files with same name
     local_filename = f"{name}-{uuid4()}{extension}"
-    # #TODO: add user-specific subdirectories, save into them based on id
-    file.save(join(current_app.config["UPLOAD_DIRECTORY"], local_filename))
+    # #TODO: maybe switch authorized upload directory's name from login to id?
+    uploader = g.user.get("login") if g.user else None
+    savepath = current_app.config["UPLOAD_DIRECTORY"]
+    if uploader:
+        savepath = join(savepath, uploader)
+        makedirs(savepath, exist_ok=True)
+
+    file.save(join(savepath, local_filename))
     savetime = datetime.utcnow()
     info = FileInfo(
         filename=local_filename,
         original_name=filename,
         uploaded=savetime,
         last_access=savetime,
+        uploader=uploader,
+        location=uploader,
     )
     db = get_db()
     db[FILES_COL_NAME].insert_one(asdict(info))
 
-    return local_filename
+    # This doesnt return uploader as uploader, but as location
+    return local_filename, uploader
 
 
-bp = Blueprint("filesharing", __name__)
-
-
-@bp.route("/uploads/<path:name>", methods=("GET", "POST"))
-def download_file(name: str) -> wrappers.Response:
-    """Get link to download specified file from server's UPLOAD_DIRECTORY"""
-
-    # updating file's last access time
+def get_download_link(name: str, directory: str = None):
+    """Get download link for provided file"""
     db = get_db()
+
+    # Updating last access to file
+    # TODO: in case file doesnt exist, it may cause trouble
     db[FILES_COL_NAME].update_one(
         {"filename": name},
         {"$set": {"last_access": datetime.utcnow()}},
     )
 
+    if directory:
+        name = join(directory, name)
+
     # Toggling "as_attachment" will make browsers open "save file" dialog instead
     # of opening direct link in new tab
-    # return send_from_directory(app.config["UPLOAD_DIRECTORY"], name, as_attachment=True)
+    # return send_from_directory(down_path, name, as_attachment=True)
     return send_from_directory(current_app.config["UPLOAD_DIRECTORY"], name)
+
+
+bp = Blueprint("filesharing", __name__)
+
+
+@bp.route("/uploads/<path:directory>/<path:name>", methods=("GET", "POST"))
+def download_from_directory(directory: str, name: str) -> wrappers.Response:
+    """Get link to download specified file from provided dir inside UPLOAD_DIRECTORY"""
+    return get_download_link(name, directory)
+
+
+@bp.route("/uploads/<path:name>", methods=("GET", "POST"))
+def download_file(name: str) -> wrappers.Response:
+    """Get link to download specified file from server's UPLOAD_DIRECTORY"""
+    return get_download_link(name)
 
 
 @bp.route("/upload", methods=("GET", "POST"))
 def upload_file():
     """File uploading form"""
 
+    # I may want to import load_logged_in_user from auth module #TODO
+    # rn save_file() works without it, but may break at some point
     if request.method == "POST":
         file = request.files.get("file", None)
         if file is None or file.filename == "":
             flash("No file has been selected")
         elif file and allowed_file(file.filename):
-            return redirect(url_for("filesharing.download_file", name=save_file(file)))
+            name, path = save_file(file)
+            if path:
+                return redirect(
+                    url_for(
+                        "filesharing.download_from_directory", name=name, directory=path
+                    )
+                )
+
+            return redirect(url_for("filesharing.download_file", name=name))
         else:
             flash("Invalid file type, please try something else")
 
